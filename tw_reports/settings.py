98be81172390b27cf4ad14edb71123b36bce9064
astromodder <22250203+astromodder@users.noreply.github.com>
2022-10-28 09:33:21 -0400

    s

--- tw_reports/settings.py
+++ tw_reports/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'tw_reports.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tw_reports',
+        'USER': 'tw_reports',
+        'PASSWORD': 'c0ns0l3',
+        'HOST': 'localhost',
     }
 }
 
